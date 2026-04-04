0720246cd73b83d219ab87fa872511d80f1b93a4
Edblas <edblas@users.noreply.github.com>
2026-04-04 11:04:18 -0300

    corrigindo

---
+++ src/main/java/com/servlink/servlink/config/MailConfig.java
@@ -0,0 +1,63 @@
+package com.servlink.servlink.config;
+
+import jakarta.mail.Session;
+import jakarta.mail.internet.MimeMessage;
+import java.io.InputStream;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    @ConditionalOnMissingBean(JavaMailSender.class)
+    public JavaMailSender javaMailSender() {
+        return new NoOpJavaMailSender();
+    }
+
+    static class NoOpJavaMailSender implements JavaMailSender {
+
+        @Override
+        public MimeMessage createMimeMessage() {
+            return new MimeMessage((Session) null);
+        }
+
+        @Override
+        public MimeMessage createMimeMessage(InputStream contentStream) {
+            try {
+                return new MimeMessage((Session) null, contentStream);
+            } catch (Exception ex) {
+                return createMimeMessage();
+            }
+        }
+
+        @Override
+        public void send(MimeMessage mimeMessage) {
+        }
+
+        @Override
+        public void send(MimeMessage... mimeMessages) {
+        }
+
+        @Override
+        public void send(MimeMessagePreparator mimeMessagePreparator) {
+        }
+
+        @Override
+        public void send(MimeMessagePreparator... mimeMessagePreparators) {
+        }
+
+        @Override
+        public void send(SimpleMailMessage simpleMessage) {
+        }
+
+        @Override
+        public void send(SimpleMailMessage... simpleMessages) {
+        }
+    }
+}
+
